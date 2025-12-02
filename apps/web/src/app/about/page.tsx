e030c4858a496dc0c28083ff2ff870339e746424
Emre Seferoğlu <yunusemre5331@icloud.com>
2025-12-03 01:12:33 +0300

    refactor: Migrate web app to `src/app` directory and set up Tailwind CSS with new UI utilities

---
+++ apps/web/src/app/about/page.tsx
@@ -0,0 +1,70 @@
+import Image, { type ImageProps } from "next/image";
+import styles from "../page.module.css";
+
+type Props = Omit<ImageProps, "src"> & {
+    srcLight: string;
+    srcDark: string;
+};
+
+const ThemeImage = (props: Props) => {
+    const { srcLight, srcDark, ...rest } = props;
+
+    return (
+        <>
+            <Image {...rest} src={srcLight} className="imgLight" />
+            <Image {...rest} src={srcDark} className="imgDark" />
+        </>
+    );
+};
+
+export default function Home() {
+    return (
+        <div className={styles.page}>
+            <ThemeImage
+                className={styles.logo}
+                srcLight="turborepo-dark.svg"
+                srcDark="turborepo-light.svg"
+                alt="Turborepo logo"
+                width={180}
+                height={38}
+                priority
+            />
+
+            <div>
+                <h1>Emre Seferoglu</h1>
+            </div>
+
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/window.svg"
+                        alt="Window icon"
+                        width={16}
+                        height={16}
+                    />
+                    Examples
+                </a>
+                <a
+                    href="https://turborepo.com?utm_source=create-turbo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/globe.svg"
+                        alt="Globe icon"
+                        width={16}
+                        height={16}
+                    />
+                    Go to turborepo.com →
+                </a>
+            </footer>
+        </div>
+    );
+}
